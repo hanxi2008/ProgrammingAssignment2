0b24bed8742f16a06a43207b62bebd49cc4bbc62
hanxi2008 <hanxi2008@gmail.com>
2015-05-28 13:50:08 +0800

    add test function

--- cachematrix.R
+++ cachematrix.R
@@ -36,3 +36,36 @@ cacheSolve <- function(x, ...) {
         x$setsolve(s)
         s
 }
+
+## testInverse will invoke the cached matrix inversion functionality provided by
+## makeCacheMatrix and cacheSolve to ensure the embedded matrix is correctly inverted.
+## It will then re-invoke cacheSolve on the existing cacheMatrix to ensure the cached
+## version of the inverse is utilized (per the "Retrieving the cached invers." message
+## from cacheSolve())
+## Input: none
+## Output: The identity matrix if the matrix was correctly inverted
+
+testInverse <- function() {
+        mat <- matrix(1:16,nrow=4,ncol=4)
+        mat[1,]=c(2,3,1,5)
+        mat[2,]=c(1,0,3,1)
+        mat[3,]=c(0,2,-3,2)
+        mat[4,]=c(0,2,3,1) 
+        print("Matrix to be inverted:")
+        print(mat)
+        if(det(mat) == 0){
+                "Matrix is not invertible"
+        } 
+        else {
+                cacheMat <- makeCacheMatrix(mat) # create a cached matrix object
+                inv <- cacheSolve(cacheMat) # compute the inverse
+                id <- inv %*% mat
+                print("Multiplication of matrix by its inverse:")
+                print(id)
+                # Copare with identity matrix
+                if(identical(id,diag(1,nrow(id),ncol(id))))
+                        print("Correct!")
+                else
+                        print("Wrong!")
+    }
+}
